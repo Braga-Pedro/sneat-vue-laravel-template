d42f87e8af234a32332c3fc98cb0f0d445b81cf6
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2025-04-15 12:41:28 -0300

    install to api, add modules

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->string('name', 300);
+            $table->string('address', 300)->nullable();
+            $table->string('phone', 25)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
