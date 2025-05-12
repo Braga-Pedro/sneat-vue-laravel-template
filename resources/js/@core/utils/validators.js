74e3109aab23b6b5290c6fc7a782b9ae395c860e
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2025-05-12 17:00:07 -0300

    Create validators.js

---
+++ resources/js/@core/utils/validators.js
@@ -0,0 +1,95 @@
+import { isEmpty, isEmptyArray, isNullOrUndefined } from './helpers'
+
+// 👉 Required Validator
+export const requiredValidator = value => {
+  if (isNullOrUndefined(value) || isEmptyArray(value) || value === false)
+    return 'Esse campo é obrigatório'
+  
+  return !!String(value).trim().length || 'Esse campo é obrigatório'
+}
+
+// 👉 Email Validator
+export const emailValidator = value => {
+  if (isEmpty(value))
+    return true
+  const re = /^(?:[^<>()[\]\\.,;:\s@"]+(?:\.[^<>()[\]\\.,;:\s@"]+)*|".+")@(?:\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\]|(?:[a-z\-\d]+\.)+[a-z]{2,})$/i
+  if (Array.isArray(value))
+    return value.every(val => re.test(String(val))) || 'O campo de email deve ser um email válido'
+  
+  return re.test(String(value)) || 'O campo de email deve ser um email válid'
+}
+
+// 👉 Password Validator
+export const passwordValidator = password => {
+  const regExp = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&*()]).{8,}/
+  const validPassword = regExp.test(password)
+  
+  return validPassword || 'A senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula, um número e um caractere especial'
+}
+
+// 👉 Confirm Password Validator
+export const confirmedValidator = (value, target) => value === target || 'As senhas não coincidem'
+
+// 👉 Between Validator
+export const betweenValidator = (value, min, max) => {
+  const valueAsNumber = Number(value)
+  
+  return (Number(min) <= valueAsNumber && Number(max) >= valueAsNumber) || `O valor deve estar entre ${min} e ${max}`
+}
+
+// 👉 Integer Validator
+export const integerValidator = value => {
+  if (isEmpty(value))
+    return true
+  if (Array.isArray(value))
+    return value.every(val => /^-?\d+$/.test(String(val))) || 'Esse campo deve ser um número inteiro'
+  
+  return /^-?\d+$/.test(String(value)) || 'Esse campo deve ser um número inteiro'
+}
+
+// 👉 Regex Validator
+export const regexValidator = (value, regex) => {
+  if (isEmpty(value))
+    return true
+  let regeX = regex
+  if (typeof regeX === 'string')
+    regeX = new RegExp(regeX)
+  if (Array.isArray(value))
+    return value.every(val => regexValidator(val, regeX))
+  
+  return regeX.test(String(value)) || 'O campo regex não é válido'
+}
+
+// 👉 Alpha Validator
+export const alphaValidator = value => {
+  if (isEmpty(value))
+    return true
+  
+  return /^[A-Z]*$/i.test(String(value)) || 'O campo deve conter apenas letras'
+}
+
+// 👉 URL Validator
+export const urlValidator = value => {
+  if (isEmpty(value))
+    return true
+  const re = /^https?:\/\/[^\s$.?#].\S*$/
+  
+  return re.test(String(value)) || 'URL inválida'
+}
+
+// 👉 Length Validator
+export const lengthValidator = (value, length) => {
+  if (isEmpty(value))
+    return true
+  
+  return String(value).length === length || `"O campo deve ter exatamente ${length} caracteres"`
+}
+
+// 👉 Alpha-dash Validator
+export const alphaDashValidator = value => {
+  if (isEmpty(value))
+    return true
+  const valueAsString = String(value)
+  
+  return /^[\w-]*$/.test(valueAsString) || 'Todos os caracteres estão inválidos'
+}
