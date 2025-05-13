bdcd67e39ffcea3f93e59085ee4d527d5dbf6391
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2025-05-13 15:24:04 -0300

    register middleware moduleControll in app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'module' => \App\Http\Middleware\ModuleControllMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
