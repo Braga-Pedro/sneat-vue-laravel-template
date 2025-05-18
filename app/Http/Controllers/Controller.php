cd638a47b7b5f39b5d83d81aa538824739767359
Pedro <139722071+Braga-Pedro@users.noreply.github.com>
2025-05-18 17:33:37 -0300

    create utilities in controller base

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,50 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function __construct()
+    {
+        $this->perPage = (int) request()->get('per_page') ?? 15;
+        $this->page = (int) (request()->get('page') ?: 1);
+        $this->returnType = request()->get('return_type') ?? 'pagination';
+        
+    }
+
+    /**
+     * @param Builder $query_or_collection
+     * @return mixed
+     */
+    public function handleCollectionOrPaginationReturn($query_or_collection, $isCollection = false)
+    {
+        if ($isCollection) {
+            $collection = collect($query_or_collection);
+
+            $total = count($query_or_collection);
+            $totalPages = ceil(($total = max(1, $total)) / $this->perPage);
+            $this->page = max(1, min($this->page, $totalPages));
+            $paginatedItems = $collection->forPage($this->page, $this->perPage)->values();
+            
+            return [
+                'current_page' => $this->page,
+                'data' => $paginatedItems,
+                'per_page' => $this->perPage,
+                'total' => $total,
+                'total_pages' => $totalPages,
+                'has_more' => $this->page < $totalPages,
+            ];
+        }
+
+        if ($this->returnType === 'pagination') {
+            return $query_or_collection->paginate($this->perPage);
+        }
+        
+        return $query_or_collection->get();
+    }
 }
